53bdd32b9700b9501fc7217cad0091f2ca110c37
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-06-28 12:30:53 +0530

    Testimonial done

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets'
 import HotelCard from './HotelCard'
+import Title from './Title'
+import { useNavigate} from 'react-router-dom'
+
+
+
 
 const FeaturedDestination = () => {
+  const navigate= useNavigate()
+
   return (
     <div className='flex flex-col items-center  px-6 md:px-16 lg:px-24 bg-slate-50 py-10'>
+
+   <Title title='Featured Destination' subTitle ='Discover the perfect getaway for your next adventure' />
+
    <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
 {roomsDummyData.slice(0,4).map((room, index)=>  (
   <HotelCard key={room._id} room={room} index={index} />  
 ))}
    </div>
 
+   <button onClick={() => {
+  navigate('/rooms');
+  setTimeout(() => scrollTo(0, 0), 50); // Give time for navigation
+}}
 
+   className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded hover:bg-gray-50 transition-all cursor-pointer'>
+    View All Destinations
+   </button>
     </div>
   )
 }
--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -4,13 +4,12 @@ import { assets } from '../assets/assets';
 
 const HotelCard = ({ room, index }) => {
   return (
-    <Link to={`/rooms/${room._id}`} onClick={() => scrollTo(0, 0)}>
-      <div className="relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]">
+    <Link to={'/rooms/' +room._id} onClick={() => scrollTo(0, 0)} key={room._id}
+      className="relative max-w-70 w-full rounded-xl overflow-hidden bg-white text-gray-500/90 shadow-[0px_4px_4px_rgba(0,0,0,0.05)]">
         <img
           src={room.images[0]}
           alt=""
-          className="w-full h-48 object-cover"
-          loading="lazy"
+          
         />
 
         {index % 4 === 0 && (
@@ -44,7 +43,7 @@ const HotelCard = ({ room, index }) => {
             </button>
           </div>
         </div>
-      </div>
+      
     </Link>
   );
 };
