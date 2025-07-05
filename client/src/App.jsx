4138ad186e4e094254ae614e36e9f3f302fd93e8
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-07-05 10:41:55 +0530

    admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,23 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import MyBookings from './pages/MyBookings';
 import RoomDetails from './pages/RoomDetails';
+import HotelReg from './components/HotelReg';
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
  { !isOwnerPath && <Navbar />}
+ <HotelReg />
 
  <div className ='min-h-[70vh]'>
   <Routes>
     <Route path ='/' element ={<Home/>} />
         <Route path ='/rooms' element ={<AllRooms />} />
                 <Route path ='/rooms/:id' element ={<RoomDetails />} />
-
+<Route path ='/my-bookings' element ={<MyBookings />} />
 
   </Routes>
 
