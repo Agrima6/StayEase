32b9d8330e4ae56e0b142e5a1049d3b6dfd9d5e4
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-07-05 19:14:39 +0530

    Done with frontend of owner side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,13 +8,17 @@ import AllRooms from './pages/AllRooms';
 import MyBookings from './pages/MyBookings';
 import RoomDetails from './pages/RoomDetails';
 import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 const App = () => {
 
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
  { !isOwnerPath && <Navbar />}
- <HotelReg />
+ {false && <HotelReg />}
 
  <div className ='min-h-[70vh]'>
   <Routes>
@@ -22,7 +26,12 @@ const App = () => {
         <Route path ='/rooms' element ={<AllRooms />} />
                 <Route path ='/rooms/:id' element ={<RoomDetails />} />
 <Route path ='/my-bookings' element ={<MyBookings />} />
+<Route path='/owner' element ={<Layout />}>
+<Route index element ={<Dashboard />} />
+<Route path='add-room' element ={<AddRoom />} />
+<Route path='list-room' element ={<ListRoom />} />
 
+</Route>
   </Routes>
 
 
