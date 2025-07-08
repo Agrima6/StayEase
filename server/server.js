028f8d1166803f739ebe30110ba299dbf16c49bc
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-07-08 19:54:03 +0530

    mongo db

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// npm i express dotenv cors mongoose cloudinary multer svix
+import express from "express"
+import "dotenv/config";
+import cors from "cors";
+
+const app =express();
+app.use(cors()); // Enable cross origin resource shairing
+
+app.get('/', (req, res)=>res.send("API is working !"))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
