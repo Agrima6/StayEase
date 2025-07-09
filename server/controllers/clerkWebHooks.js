4544c0c067f3e5b7f62bcd55d42d68d28265ed10
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-07-09 22:28:16 +0530

    vercel

--- server/controllers/clerkWebHooks.js
+++ server/controllers/clerkWebHooks.js
@@ -15,7 +15,45 @@ const clerkWebhooks = async (req, res) => {
 
 
         };
+        // verifying header
+        await whook.verify(JSON.stringify(req.body), header);
+        // getting data from request body
+        const {data , type} = req.body;
+
+      const userData = {
+        _id: data.id,
+        email: data.email_addresses[0].email_address,
+        username: data.first_name + " " + data.last_name,
+        image: data.image_url,
+      }
+      // Switch case for difefrent events
+// getting these events in case from clerk
+      switch(type) {
+        case "user.created":{
+            await User.create(userData)
+            break;
+        }
+
+         case "user.updated":{
+            await User.findByIdAndUpdate(data.id, userData)
+            break;
+        }
+
+          case "user.deleted":{
+            await User.findByIdAndDelete(data.id)
+            break;
+        }
+        res.json({success: true, message: "WebHook Recieved"})
+
+        default:
+            break;
+      }
+
     } catch(error){
+        console.log(error.message);
+        res.json({success: false, message: error.message })
 
     }
 }
+
+export default clerkWebhooks
