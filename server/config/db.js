7e91fdd705309b09eaa33df5521d542612394346
Agrima6 <agrima.agarwal.23cse@bmu.edu.in>
2025-07-09 19:45:30 +0530

    models and controllers folder

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try{
+        mongoose.connection.on('connected', ()=> console.log("Data base connected"));
         await mongoose.connect(`${process.env.MONGODB_URI}/StayEase`)
     } catch(error){
-        console.log(error.)
+        console.log(error.message);
     }
 }
+
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@
 import express from "express"
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import { clerkMiddleware } from '@clerk/express'
+
+connectDB()
 
 const app =express();
 app.use(cors()); // Enable cross origin resource shairing
+
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
 
 app.get('/', (req, res)=>res.send("API is working !"))
 const PORT = process.env.PORT || 3000;
